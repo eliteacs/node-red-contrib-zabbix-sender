43bb2a11864781e073e786a4bdf0ee455d3a85d4
Jose Luiz Vieira <31421430+eliteacs@users.noreply.github.com>
2021-03-05 13:12:37 -0300

    Update zabbix-sender.js

--- zabbix-sender.js
+++ zabbix-sender.js
@@ -11,8 +11,8 @@ module.exports = function (RED) {
 		this.on('input', function (msg) {
 			var data = msg.payload
 			var sender = new ZabbixSender({
-				host: msg.zabbixServer,
-				port: msg.zabbixPort,
+				host: node.config.zabbixServer || msg.zabbixServer,
+				port: node.config.zabbixPort ||msg.zabbixPort,
 				timeout: node.config.timeout,
 				with_ns: node.config.withNs,
 				with_timestamps: node.config.sendTimestamps,
